a50b06a983b579258e612a507add311c1e5973b7
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 11:37:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,21 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  
+  def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 end
 
 
@@ -10,3 +25,4 @@ end
   else
     return false
   end
+end
